92b7e0c54f6f09ead6bb09e33124809dacea3d6a
blee <dlqudgus2587@gmail.com>
2022-07-22 06:39:19 +0000

    apply codebook ++ one codebook for each channel(Not shared within channels)

--- models/grid_based.py
+++ models/grid_based.py
@@ -8,7 +8,7 @@ import models.cosine_transform as ct
 
 class FreqGrid(nn.Module):
     def __init__(self, resolution: int, n_chan: int, n_freq=None,
-                 freq_resolution=None):
+                 freq_resolution=None, bitwidth=3, grid_num=1):
         # assume 3 axes have the same resolution
         super().__init__()
         self.resolution = resolution
@@ -24,16 +24,23 @@ class FreqGrid(nn.Module):
         self.freqs = nn.Parameter(torch.linspace(0., 1, self.n_freq),
                                   requires_grad=False)
 
-        self.grid = nn.Parameter(nn.Parameter(
-            torch.zeros(3, n_chan*self.n_freq, resolution, resolution),
-            requires_grad=True))
+        # self.grid = nn.Parameter(nn.Parameter(
+        #     torch.zeros(3, n_chan*self.n_freq, resolution, resolution),
+        #     requires_grad=True))
+
+        # Assume that each channel has its own codebook
+
+        self.bitwidth = bitwidth
+        self.codebook = nn.Parameter(torch.normal(0, 0.1, size=(3, 2**bitwidth, self.n_chan * self.n_freq)), requires_grad=True)
+        self.indices = nn.Parameter(torch.zeros(3, resolution * resolution, 2**bitwidth), requires_grad=True) # Tensor C in Variable Bitrate
+
 
     def forward(self, coords):
         # [B, 3] to [1, B, 1, 3]
-        coords = coords.reshape(1, -1, 1, coords.shape[-1])
+        coords = coords.reshape(1, -1, 1, coords.shape[-1]) 
 
         # coefs: [3, 1, C, B]
-        grid = self.grid
+        grid = self.get_grid()
         coefs = F.grid_sample(grid, torch.cat([coords[..., (1, 2)],
                                                coords[..., (0, 2)],
                                                coords[..., (0, 1)]], 0),
@@ -62,11 +69,17 @@ class FreqGrid(nn.Module):
 
     def compute_tv(self):
         weight = self.get_freqs().repeat(self.n_chan).reshape(-1, 1, 1)
-        return (self.grid * weight).square().mean()
+        # return (self.grid * weight).square().mean()
+        return (self.get_grid() * weight).square().mean()
 
     def get_freqs(self):
         return -1 + 2**(self.freqs.clamp(min=0, max=1)
                         * np.log2(self.freq_resolution))
+
+    def get_grid(self):
+        softened_mat = torch.softmax(self.indices, dim=2)
+        grid = softened_mat @ self.codebook
+        return grid.view(3, -1, self.resolution, self.resolution)
 
 
 class PREF(nn.Module):
